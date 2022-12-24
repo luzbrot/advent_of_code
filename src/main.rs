d7811dd798c3c220c2f04f29c871f2888c23ae12
luzbrot <none>
2022-12-24 13:07:24 +0000

    Added Day 14 of 2022

--- src/main.rs
+++ src/main.rs
@@ -1,206 +1,191 @@
-use std::{fs, cmp::Ordering};
+use std::{fs, ops::{Add, AddAssign}};
 
 
-/// A Packet can be a value or a list of Packets
-#[derive(Debug, Clone)]
-enum Packet {
-    Value(u32),
-    List(Vec<Packet>)
+/// The possible tile types
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Tiles {
+    Air,
+    Sand,
+    Rock
 }
-impl Packet {
-    /// Promote A Value to a List containing this Value.
-    /// A List will be returned as is
-    fn promote(&self) -> Self {
-        match self {
-            Self::List(_) => self.clone(),
-            Self::Value(_) => Self::List(vec![self.clone()])
+impl Default for Tiles {
+    fn default() -> Self {
+        Tiles::Air
+    }
+}
+impl Default for &Tiles {
+    fn default() -> Self {
+        &Tiles::Air
+    }
+}
+
+/// A vector for the positions
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct Vector {
+    x: i32, 
+    y: i32
+}
+impl Vector {
+    /// Calculate the direction to an other vec under the assumption that directions
+    /// are always straight vertical or horizontal (Puzzle description).
+    /// 
+    /// # Arguments
+    /// * `to` - The other vector to calculate the direction to
+    fn direction_to(&self, to: &Vector) -> Self {
+        Self {
+            x: (to.x - self.x).signum(),
+            y: (to.y - self.y).signum()
         }
     }
 }
-impl From<&str> for Packet {
+impl From<&str> for Vector {
     fn from(s: &str) -> Self {
-        if s.starts_with('[') {
-            let mut vec = Vec::new();
-            let mut stripped = s.strip_prefix('[').unwrap().strip_suffix(']').unwrap();
-            while !stripped.is_empty() {
-                let iter = stripped.chars().enumerate();
-                let mut parsing_list = 0;
-                let mut done_something = false;
-                for (i, c) in iter {
-                    if c == ',' {
-                        if parsing_list > 0 {continue;}
-                        vec.push(stripped[0..i].into());
-                        if i + 1 < stripped.len() {
-                            stripped = &stripped[(i+1)..];
-                        }
-                        else {
-                            stripped = "";
-                        }
-                        done_something = true;
-                        break;
-                    }
-                    else if c == '[' {
-                        parsing_list += 1;
-                    }
-                    else if c == ']' {
-                        parsing_list -= 1;
-                        if parsing_list == 0 {
-                            vec.push(stripped[0..(i+1)].into());
-                            if i + 2 < stripped.len() {
-                                stripped = &stripped[(i+2)..];
-                            }
-                            else {
-                                stripped = "";
-                            }
-                            done_something = true;
-                            break;
-                        }
-                    }
-                }
-                if !done_something {
-                    vec.push(stripped.into());
-                    break;
-                }
-            }
-            Packet::List(vec)
+        let mut parts = s.split(',');
+        Self {
+            x: parts.next().unwrap().parse().unwrap(),
+            y: parts.next().unwrap().parse().unwrap()
         }
-        else {
-            Packet::Value(s.parse().unwrap())
+    }
+}
+impl Add for Vector {
+    type Output = Vector;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y
         }
+    }
+}
+impl AddAssign for Vector {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
     }
 }
 
-impl Eq for Packet {}
-impl PartialEq for Packet {
-    fn eq(&self, other: &Self) -> bool {
-        match self {
-            Packet::List(list) => {
-                match other {
-                    Packet::List(other_list) => {
-                        let iter = list.iter().zip(other_list.iter());
-                        let mut skipped_iter = iter.skip_while(|el| el.0 == el.1);
-                        if let Some(el) = skipped_iter.next() {
-                            el.0.eq(el.1)
-                        }
-                        else {
-                            list.len() == other_list.len()
-                        }
-                        
-                    },
-                    Packet::Value(_) => {
-                        self.eq(&other.promote())
-                    }
-                }
-            },
-            Packet::Value(val) => {
-                match other {
-                    Packet::List(_) => {
-                        self.promote().eq(other)
-                    },
-                    Packet::Value(other_val) => {
-                        val.eq(other_val)
-                    }
-                }
-            }
-        }
-    }
-}
-
-impl Ord for Packet {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match self {
-            Packet::List(list) => {
-                match other {
-                    Packet::List(other_list) => {
-                        let iter = list.iter().zip(other_list.iter());
-                        let mut skipped_iter = iter.skip_while(|el| el.0 == el.1);
-                        if let Some(el) = skipped_iter.next() {
-                            el.0.cmp(el.1)
-                        }
-                        else if list.len() < other_list.len() {
-                            Ordering::Less
-                        }
-                        else if list.len() > other_list.len() {
-                            Ordering::Greater
-                        }
-                        else {
-                            Ordering::Equal
-                        }
-                        
-                    },
-                    Packet::Value(_) => {
-                        self.cmp(&other.promote())
-                    }
-                }
-            },
-            Packet::Value(val) => {
-                match other {
-                    Packet::List(_) => {
-                        self.promote().cmp(other)
-                    },
-                    Packet::Value(other_val) => {
-                        val.cmp(other_val)
-                    }
-                }
-            }
-        }
-    }
-}
-impl PartialOrd for Packet {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-/// A pair of two Packets
-#[derive(Debug)]
-struct Pair(Packet, Packet);
-impl Pair {
-    /// Check if the two packets a correctly ordered (first < second)
-    fn is_correctly_ordered(&self) -> bool {
-        self.0 < self.1
-    }
-}
-impl From<&str> for Pair {
+/// A line with multiple segments. Each segment is from the previous position to the next.
+struct Line(Vec<Vector>);
+impl From<&str> for Line {
     fn from(s: &str) -> Self {
-        let mut parts = s.split('\n');
-        Self(
-            parts.next().unwrap().into(),
-            parts.next().unwrap().into()
-        )
-    }
-}
+        let mut vec =  Vec::new();
 
-/// Pairs a renamed vec of pairs
-#[derive(Debug)]
-struct Pairs(Vec<Pair>);
-impl Pairs {
-    /// Part 1
-    fn get_sum_of_index_of_correctly_ordered_packets(&self) -> usize {
-        self.0.iter().enumerate().filter(|el| el.1.is_correctly_ordered()).map(|el| el.0 + 1).sum()
-    }
-    /// Part 2
-    /// Adds a Pair to the vec.
-    fn get_decoder_key(&mut self) -> usize {
-        let first_divider: Packet = "[[2]]".into();
-        let second_divider: Packet = "[[6]]".into();
-        self.0.push(Pair(first_divider.clone(), second_divider.clone()));
-
-        let mut packets: Vec<Packet> = self.0.iter().flat_map(|el| vec![el.0.clone(), el.1.clone()]).collect();
-        packets.sort();
-
-        packets.iter().enumerate().filter(|el| el.1 == &first_divider || el.1 == &second_divider).map(|el| el.0 + 1).product()
-    }
-}
-impl From<&str> for Pairs {
-    fn from(s: &str) -> Self {
-        let mut vec = Vec::new();
-        let parts = s.split("\n\n");
-        for p in parts {
-            vec.push(p.into());
+        for part in s.split(" -> ") {
+            vec.push(part.into());
         }
 
         Self(vec)
+    }
+}
+
+/// A cave saving the tiles in a cave and the start position of the sand 
+#[derive(Debug)]
+struct Cave {
+    /// The tiles in the cave.
+    /// If a position is not set the default is assumed.
+    /// This can lead to rows of just default tiles to be an empty Vec.
+    tiles: Vec<Vec<Tiles>>,
+    /// The start/spawn position of the sand
+    sand_entry: Vector
+}
+impl Cave {
+    /// Create a new cave with just default tiles
+    fn new() -> Self {
+        Self {
+            tiles: Vec::new(),
+            sand_entry: Vector { x: 500, y: 0 }
+        }
+    }
+    /// Draw a line of rock in the cave
+    /// 
+    /// # Arguments
+    /// * `line` - The line to draw with rock
+    fn draw_line_of_rock(&mut self, line: &Line) {
+        let lefts = line.0.iter().take(line.0.len() - 1);
+        let rights = line.0.iter().skip(1);
+        for line_part in lefts.zip(rights) {
+            let dir = line_part.0.direction_to(line_part.1);
+            let mut pos = *line_part.0;
+            while pos != *line_part.1 {
+                self.add_tile(Tiles::Rock, pos);
+                pos += dir;
+            }
+            self.add_tile(Tiles::Rock, pos);
+        }
+    }
+    /// Adds a tile in the cave of the specified type.
+    /// Only needed positions are populated.
+    /// 
+    /// # Arguments
+    /// * `tile` - The tile to add to the cave
+    /// * `pos` - The position to add the tile to
+    fn add_tile(&mut self, tile: Tiles, pos: Vector) {
+        for _ in self.tiles.len()..(pos.y as usize + 1) {
+            self.tiles.push(Vec::new());
+        }
+        for _ in self.tiles[pos.y as usize].len()..(pos.x as usize + 1) {
+            self.tiles[pos.y as usize].push(Tiles::default());
+        }
+        self.tiles[pos.y as usize][pos.x as usize] = tile;
+    }
+    /// Spawn sand at the spawn position and find the position where it comes to rest.
+    /// Does nothing if the spawn position is blocked.
+    /// 
+    /// # Returns
+    /// Returns true if the sand comes to rest.
+    /// Returns false if the sand will come not to rest or if the spawn position is blocked.
+    fn spawn_sand(&mut self) -> bool {
+        let mut pos = self.sand_entry;
+        let sand_can_move = |pos: &mut Vector| -> bool {
+            if let Some(col) = self.tiles.get(pos.y as usize + 1) {
+                let down = col.get(pos.x as usize).unwrap_or_default();
+                if *down == Tiles::Air {
+                    pos.y += 1;
+                    return true;
+                }
+                let down_left = col.get(pos.x as usize - 1).unwrap_or_default();
+                if *down_left == Tiles::Air {
+                    pos.y += 1;
+                    pos.x -= 1;
+                    return true;
+                }
+                let down_right = col.get(pos.x as usize + 1).unwrap_or_default();
+                if *down_right == Tiles::Air {
+                    pos.y += 1;
+                    pos.x += 1;
+                    return true;
+                }
+                false
+            }
+            else {
+                pos.y += 1;
+                true
+            }
+        };
+
+        if *self.tiles[pos.y as usize].get(pos.x as usize).unwrap_or_default() == Tiles::Sand {
+            // Entry blocked
+            return false;
+        }
+        while sand_can_move(&mut pos) {
+            if pos.y as usize >= self.tiles.len() {
+                break;
+            }
+        }
+        if !sand_can_move(&mut pos) {
+            self.add_tile(Tiles::Sand, pos);
+            true
+        }
+        else {
+            false
+        }
+    }
+    /// Add a floor two rows under the lowest rows in the cave.
+    fn add_floor(&mut self) {
+        let y = self.tiles.len() + 1;
+
+        for x in 0..(self.sand_entry.x * 2) {
+            self.add_tile(Tiles::Rock, Vector { x, y: y as i32});
+        }
     }
 }
 
@@ -208,53 +193,60 @@ impl From<&str> for Pairs {
 fn main() {
     let input = fs::read_to_string("input").unwrap_or_else(|_| panic!("Unable to read input"));
 
-    let mut pairs: Pairs = input.as_str().into();
-    println!("The sum of indexes is {}", pairs.get_sum_of_index_of_correctly_ordered_packets());
-    println!("The decoder key is {}", pairs.get_decoder_key());
+    let mut cave = Cave::new();
+    let mut cave_with_flor = Cave::new();
+
+    for line in input.split('\n') {
+        let line: Line = line.into();
+        cave.draw_line_of_rock(&line);
+        cave_with_flor.draw_line_of_rock(&line);
+    }
+    cave_with_flor.add_floor();
+
+    let mut i = 0;
+    while cave.spawn_sand() {
+        i += 1;
+    }
+    println!("There {} units of sand at rest", i);
+
+    i = 0;
+    while cave_with_flor.spawn_sand() {
+        i += 1;
+    }
+    println!("There are {} units sand until the entry is blocked", i);
 }
 
 
 #[cfg(test)]
 mod tests {
-    use crate::{Pairs, Pair};
-
+    use crate::{Cave, Line};
 
     #[test]
     fn check_against_example() {
-        let p1: Pair = "[[1]]
-[1]".into();
-        assert!(p1.0 == p1.1);
-        let p2: Pair = "[[1],4]
-[1,[2,[3,[4,[5,6,0]]]],8,9]".into();
-        assert!(!p2.is_correctly_ordered());
+        let input = "498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9";
 
+        let mut cave = Cave::new();
+        let mut cave_with_floor = Cave::new();
 
-        let input = "[1,1,3,1,1]
-[1,1,5,1,1]
+        for line in input.split('\n') {
+            let line: Line = line.into();
+            cave.draw_line_of_rock(&line);
+            cave_with_floor.draw_line_of_rock(&line);
+        }
 
-[[1],[2,3,4]]
-[[1],4]
+        cave_with_floor.add_floor();
 
-[9]
-[[8,7,6]]
+        let mut i = 0;
+        while cave.spawn_sand() {
+            i += 1;
+        }
+        assert_eq!(i, 24);
 
-[[4,4],4,4]
-[[4,4],4,4,4]
-
-[7,7,7,7]
-[7,7,7]
-
-[]
-[3]
-
-[[[]]]
-[[]]
-
-[1,[2,[3,[4,[5,6,7]]]],8,9]
-[1,[2,[3,[4,[5,6,0]]]],8,9]";
-
-        let mut pairs: Pairs = input.into();
-        assert_eq!(pairs.get_sum_of_index_of_correctly_ordered_packets(), 13);
-        assert_eq!(pairs.get_decoder_key(), 140);
+        i = 0;
+        while cave_with_floor.spawn_sand() {
+            i += 1;
+        }
+        assert_eq!(i, 93);
     }
 }
